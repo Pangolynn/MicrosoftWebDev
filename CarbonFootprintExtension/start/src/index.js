ec1e408274c9b64bd9513f3aed008345e68f83c9
Sammi <sami.berlynn@proton.me>
2025-01-11 23:29:16 +0100

    setup js form fields, results, and init function

--- CarbonFootprintExtension/start/src/index.js
+++ CarbonFootprintExtension/start/src/index.js
@@ -1,6 +1,16 @@
 //1
 // form fields
+const form = document.querySelector('.form-data');
+const region = document.querySelector('.region-name');
+const apiKey = document.querySelector('.api-key');
 // results divs
+const errors = document.querySelector('.errors');
+const loading = document.querySelector('.loading');
+const results = document.querySelector('.result-container');
+const usage = document.querySelector('.carbon-usage');
+const fossilfuel = document.querySelector('.fossil-fuel');
+const myregion = document.querySelector('.my-region');
+const clearBtn = document.querySelector('.clear-btn');
 
 //6
 //call the API
@@ -12,6 +22,32 @@
 // handle form submission
 
 //3 initial checks
+function init() {
+    //check localStorage
+    const storedApiKey = localStorage.getItem('apiKey');
+    const storedRegion = localStorage.getItem('regionName');
+    
+    
+    if (storedApiKey === null || storedRegion === null) {
+        // we don't have keys, show form
+        form.style.display = 'block';
+        results.style.display = 'none';
+        loading.style.display = 'none';
+        clearBtn.style.display = 'none';
+        errors.textContent = '';
+    } else {
+        // we have keys, show results
+        displayCarbonUsage(storedApiKey, storedRegion);
+        results.style.display= 'none';
+        form.style.display = 'none';
+        clearBtn.style.display = 'block';
+    }
+};
+
 
 //2
 // set listeners and start app
+form.addEventListener('submit', (e) => handleSubmit(e));
+clearBtn.addEventListener('click', (e) => reset(e));
+init();
+
