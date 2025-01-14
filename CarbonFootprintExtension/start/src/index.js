f1f668e2ef583ede9fbfdc17339a58bee78cdfa2
Sammi <sami.berlynn@proton.me>
2025-01-14 23:53:57 +0100

    added color calculator

--- CarbonFootprintExtension/start/src/index.js
+++ CarbonFootprintExtension/start/src/index.js
@@ -49,6 +49,22 @@ async function displayCarbonUsage(apiKey, region) {
     }
 }
 
+calculateColor = async (value) => {
+	let co2Scale = [0, 150, 600, 750, 800];
+	let colors = ['#2AA364', '#F5EB4D', '#9E4229', '#381D02', '#381D02'];
+
+	let closestNum = co2Scale.sort((a, b) => {
+		return Math.abs(a - value) - Math.abs(b - value);
+	})[0];
+	//console.log(value + ' is closest to ' + closestNum);
+	let num = (element) => element > closestNum;
+	let scaleIndex = co2Scale.findIndex(num);
+
+	let closestColor = colors[scaleIndex];
+	//console.log(scaleIndex, closestColor);
+
+	chrome.runtime.sendMessage({ action: 'updateIcon', value: { color: closestColor } });
+};
 
 //5
 //set up user's api key and region
