41692db1454ee22d2feea291f4e6fc4ae5994d07
Sammi <sami.berlynn@proton.me>
2025-01-17 23:52:38 +0100

    Added starting files for space game

---
+++ SpaceGame/app.js
@@ -0,0 +1,29 @@
+function loadTexture(path) {
+  return new Promise((resolve) => {
+    const img = new Image()
+    img.src = path
+    img.onload = () => {
+      resolve(img)
+    }
+  })
+}
+
+function createEnemies(ctx, canvas, enemyImg) {
+  // TODO draw enemies
+}
+
+window.onload = async () => {
+  canvas = document.getElementById('canvas')
+  ctx = canvas.getContext('2d')
+  const width = canvas.width;
+  const height = canvas.height;
+  // TODO load textures
+
+  // TODO draw black background
+  ctx.fillStyle = 'black';
+
+  ctx.fillRect(0,0, width, height);
+  // TODO draw hero
+  // TODO uncomment the next line when you add enemies to screen
+  //createEnemies(ctx, canvas, enemyImg);
+}
