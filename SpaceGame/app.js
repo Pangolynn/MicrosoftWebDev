e7d7fac47938d4b4d2942876a0fe24fe85351def
Sammi <sami.berlynn@proton.me>
2025-01-22 21:48:52 +0100

    both player and enemies loaded to screen now

--- SpaceGame/app.js
+++ SpaceGame/app.js
@@ -5,11 +5,20 @@ function loadTexture(path) {
     img.onload = () => {
       resolve(img)
     }
-  })
+  });
 }
 
 function createEnemies(ctx, canvas, enemyImg) {
-  // TODO draw enemies
+  const MONSTER_TOTAL = 5;
+  const MONSTER_WIDTH = MONSTER_TOTAL * 98;
+  const START_X = (canvas.width - MONSTER_WIDTH) / 2;
+  const STOP_X = START_X + MONSTER_WIDTH;
+
+  for (let x = START_X; x < STOP_X; x += 98) {
+    for (let y = 0; y < 50 * 5; y += 50) {
+      ctx.drawImage(enemyImg, x, y);
+    }
+  }
 }
 
 window.onload = async () => {
@@ -18,13 +27,12 @@ window.onload = async () => {
   const width = canvas.width;
   const height = canvas.height;
 
-  const heroImg = await loadTexture('assets/player.png');
-  const monsterImg = await loadTexture('assets/enemyShip.png');
+  const playerImg = await loadTexture('assets/player.png');
+  const enemyImg = await loadTexture('assets/enemyShip.png');
 
   ctx.fillStyle = 'black';
 
   ctx.fillRect(0,0, width, height);
-  ctx.drawImage(heroImg, canvas.width / 2 - 45, canvas.height - canvas.height / 4 );
-  // TODO uncomment the next line when you add enemies to screen
-  //createEnemies(ctx, canvas, enemyImg);
+  ctx.drawImage(playerImg, canvas.width / 2 - 45, canvas.height - canvas.height / 4 );
+  createEnemies(ctx, canvas, enemyImg);
 }
